760fcf383f0685b33e511526d0513b2253755640
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:37:15 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -5,15 +5,9 @@ $mysql_user = 'username';
 $mysql_pass = 'Stoley317';
 $mysql_db = 'a.database';
 $conn_error = 'couldnot conenct to database';
-$conn = new mysqli($mysql_host, $$mysql_user, $mysql_pass, $mysql_db);
-
-if($conn ->connect_error){
-	die("conention failed: " . $conn->connect_error);
+// || !@mysql_select_db($mysql_db)
+if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass)){
+	die($conn_error);
 }
-echo "connected";
-
-//if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysql_select_db($mysql_db)){
-//	die($conn_error);
-//}
 
 ?>
