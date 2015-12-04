89f8da857a181b110a25ef67787f3f4c30d9b238
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:18:49 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -3,7 +3,7 @@
 $mysql_host = 'robotsvsaliens.database.windows.net';
 $mysql_user = 'unsername';
 $mysql_pass = 'Password123';
-$mysql_db = 'a_database';
+$mysql_db = 'a.database';
 $conn_error = 'couldnot conenct to database';
 
 if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysql_select_db($mysql_db)){
