5dda2094040174ad59a23e21305f2ad06bf407af
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:50:20 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,10 +1,10 @@
 <?php
 
-$mysql_host = 'robotsvsaliens';
+$mysql_host = 'robotsvsaliens.database.windows.net';
 $mysql_user = 'username';
 $mysql_pass = 'Password123';
 $mysql_db = 'a.database';
-$conn_error = 'couldnot conenct to database';
+$conn_error = 'YOU FUCKING SUCK';
 // || !@mysql_select_db($mysql_db)
 if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass)){
 	die($conn_error);
