f1c9cd505fd51bff1cbc4ec248e653eb6c082fe5
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:53:02 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -5,8 +5,8 @@ $mysql_user = 'username';
 $mysql_pass = 'Password123';
 $mysql_db = 'a.database';
 $conn_error = 'YOU FUCKING SUCK';
-// || !@mysql_select_db($mysql_db)
-if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass)){
+
+if(!mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !mysql_select_db($mysql_db)){
 	die($conn_error);
 }
 
