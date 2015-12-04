41e3c3538762fbdfe68edd0b2f522caa80046e7d
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:35:09 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -1,13 +1,19 @@
 <?php
 
-$mysql_host = 'robotsvsaliens';
+$mysql_host = 'robotsvsaliens.database.windows.net';
 $mysql_user = 'username';
 $mysql_pass = 'Stoley317';
 $mysql_db = 'a.database';
 $conn_error = 'couldnot conenct to database';
+$conn = new mysqli($mysql_host, $$mysql_user, $mysql_pass, $mysql_db);
 
-if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysql_select_db($mysql_db)){
-	die($conn_error);
+if($conn ->connect_error){
+	die("conention failed: " . $conn->connect_error);
 }
+echo "connected";
+
+//if(!@mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !@mysql_select_db($mysql_db)){
+//	die($conn_error);
+//}
 
 ?>
