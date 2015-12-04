c8150582a713aaaaa6838c04bd8434c91960824d
codyweis <codyweisenberger@gmail.com>
2015-12-04 15:53:15 -0700

    Update connect.inc.php

--- connect.inc.php
+++ connect.inc.php
@@ -4,7 +4,7 @@ $mysql_host = 'robotsvsaliens.database.windows.net';
 $mysql_user = 'username';
 $mysql_pass = 'Password123';
 $mysql_db = 'a.database';
-$conn_error = 'YOU FUCKING SUCK';
+$conn_error = 'YOU  SUCK';
 
 if(!mysql_connect($mysql_host, $mysql_user, $mysql_pass) || !mysql_select_db($mysql_db)){
 	die($conn_error);
